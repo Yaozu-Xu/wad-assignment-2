4153f2d99b01c3b898e1bbc64e34b10ac79f3a68
Yaozu-Xu <2589231996@qq.com>
2020-12-23 11:38:56 +0000

    Build: Add deploy settings on netlify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import express from 'express'
+import serverless from 'serverless-http'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
 
+const router = express.Router()
 const port = process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello'))
+router.get('/', (req, res) => res.json({ hello: 'xyz' }))
 
-app.listen(port, () => {
-  console.info(`Server running at ${port}`)
-})
+app.use('./netlify/functions/api', router)
+
+app.listen(port)
+
+module.exports.handler = serverless(app)
