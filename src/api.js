b61b8983f347f98639c2bcfca5a7dafd896c2cc6
Yaozu-Xu <2589231996@qq.com>
2020-12-24 21:34:01 +0000

    Refactor: Rename entrance file and fetch seed data

--- src/api.js
+++ src/api.js
@@ -1,13 +1,20 @@
+import './db'
 import express from 'express'
 import dotenv from 'dotenv'
 import serverless from 'serverless-http'
 import movieRouter from './routes/movies'
+import { loadMovies } from './seed'
 
 dotenv.config()
 
+if (process.env.SEED_DATA === 'development') {
+  loadMovies()
+}
+
 const app = express()
 
-app.use('/.netlify/functions/index', movieRouter)
+app.use('/.netlify/functions/api/movies', movieRouter)
 
-// eslint-disable-next-line import/prefer-default-export
 export const handler = serverless(app)
+
+module.exports = app
--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import movieModel from '../../models/movieModel'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ movie: 'test' })
+router.get('/', (req, res, next) => {
+  movieModel.find().then((movies) => res.status(200).send(movies)).catch(next)
 })
 
 router.get('/test', (req, res) => {
