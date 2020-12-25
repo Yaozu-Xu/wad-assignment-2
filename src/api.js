5339fccfce34bedc8fd42613b49499e59d065b8e
Yaozu-Xu <2589231996@qq.com>
2020-12-25 17:06:26 +0000

    Feature: Finish api of fetching movie stars

--- src/api.js
+++ src/api.js
@@ -3,17 +3,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import serverless from 'serverless-http'
 import movieRouter from './routes/movies'
-import { loadMovies } from './seed'
+import starRouter from './routes/stars'
+import { loadMovies, loadStars } from './seed'
 
 dotenv.config()
 
 if (process.env.SEED_DATA === 'development') {
   loadMovies()
+  loadStars()
 }
 
 const app = express()
 
 app.use('/.netlify/functions/api/movies', movieRouter)
+app.use('/.netlify/functions/api/stars', starRouter)
 
 export const handler = serverless(app)
 
---
+++ src/routes/stars/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import starModel from '../../models/starModel'
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  starModel.find().then((movies) => res.status(200).send(movies)).catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  // eslint-disable-next-line radix
+  const id = parseInt(req.params.id)
+  starModel.findStarById(id).then((star) => res.status(200).send(star)).catch(next)
+})
+
+router.get('/test', (req, res) => {
+  res.json({ test: 'test' })
+})
+
+export default router
