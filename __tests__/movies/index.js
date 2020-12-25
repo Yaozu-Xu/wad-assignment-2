777fcb44af8976d8f101a2d16bc233de46dfc09c
Yaozu-Xu <2589231996@qq.com>
2020-12-25 19:45:33 +0000

    Test: Refactor movie api tests

--- __tests__/movies/index.js
+++ __tests__/movies/index.js
@@ -4,14 +4,48 @@ import api from '../../src/api'
 
 const baseUrl = '/.netlify/functions/api/movies'
 
-describe('movie api testing', () => {
-  it('get the movie list api', async (done) => {
+const id = 590706
+
+const movie = {
+  adult: false,
+  backdrop_path: '/jeAQdDX9nguP6YOX6QSWKDPkbBo.jpg',
+  genre_ids: [
+    28,
+    14,
+    878,
+  ],
+  id: 590706,
+  original_language: 'en',
+  original_title: 'Jiu Jitsu',
+  overview: 'Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of alien invaders. But when a celebrated war hero goes down in defeat, the fate of the planet and mankind hangs in the balance.',
+  popularity: 2633.943,
+  poster_path: '/eLT8Cu357VOwBVTitkmlDEg32Fs.jpg',
+  release_date: '2020-11-20',
+  title: 'Jiu Jitsu',
+  video: false,
+  vote_average: 5.9,
+  vote_count: 111,
+}
+
+describe('movie api get request testing', () => {
+  it('should get the movie list', async (done) => {
     await request(api)
       .get(baseUrl)
       .expect('Content-Type', /json/)
       .expect(200)
       .then((res) => {
         expect(res.body.length).toBeGreaterThan(0)
+      })
+    done()
+  })
+  it('should get the specfied movie', async (done) => {
+    await request(api)
+      .get(`${baseUrl}/${id}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.id).toBe(movie.id)
+        expect(res.body.backdrop_pat).toBe(movie.backdrop_pat)
       })
     done()
   })
