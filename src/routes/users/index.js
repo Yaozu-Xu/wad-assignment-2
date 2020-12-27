0202aadbff4230dcdd42e415f1f4bcf3c8aad712
Yaozu-Xu <2589231996@qq.com>
2020-12-27 14:34:01 +0000

    Feature: Add auth route

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 import { body, validationResult } from 'express-validator'
 import User from '../../models/userModel'
 
@@ -13,14 +14,14 @@ router.get('/:username', (req, res, next) => {
   User.findByUserName(username).then((users) => res.status(200).json(users)).catch(next)
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params
   const hasUser = await User.findById(id)
   if (!hasUser) {
     return res.status(400).json({ err: 'no such user' })
   }
   await User.deleteOne({ _id: id })
-  return res.status(200).json({ msg: 'update user successfully' }).catch(next)
+  return res.status(200).json({ msg: 'update user successfully' })
 })
 
 // create new user
@@ -39,6 +40,30 @@ router.post('/register',
       msg: 'Successful created new user',
     })
   })
+
+router.post('/auth', async (req, res, next) => {
+  const user = await User.findByUserName(req.body.username).catch(next)
+  if (!user) {
+    return res.status(401).json({
+      code: 401,
+      msg: 'Authentication failed. User not found.',
+    })
+  }
+  await user.comparePassword(req.body.password, (err, isMatch) => {
+    if (isMatch && !err) {
+      const token = jwt.sign(user.username, process.env.SALT)
+      res.status(200).json({
+        success: true,
+        token,
+      })
+    } else {
+      res.status(401).json({
+        code: 401,
+        msg: 'Authentication failed. Wrong password.',
+      })
+    }
+  })
+})
 
 // update registered user info
 router.put('/put/:id',
